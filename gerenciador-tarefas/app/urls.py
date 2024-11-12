5fdca50e0a827a464f6995c7b9d7192651d2bc86
Matheus Fagundes <119632707+theuzs@users.noreply.github.com>
2024-11-11 21:35:24 -0300

    Add files via upload

---
+++ gerenciador-tarefas/app/urls.py
@@ -0,0 +1,18 @@
+from django.urls import path
+from .views.tarefa_views import (
+    listar_tarefas, cadastrar_tarefa, editar_tarefa, remover_tarefa
+)
+from .views.usuario_views import (
+    cadastrar_usuario, logar_usuario, deslogar_usuario, dashboard_view
+)
+
+urlpatterns = [
+    path('', logar_usuario, name='logar_usuario'),  # Página de login
+    path('dashboard/', dashboard_view, name='dashboard'),  # Página do dashboard
+    path('tarefas/', listar_tarefas, name='listar_tarefas'),  # Lista de tarefas
+    path('tarefas/cadastrar/', cadastrar_tarefa, name="cadastrar_tarefa"),  # Cadastro de tarefa
+    path('tarefas/editar/<int:id>/', editar_tarefa, name="editar_tarefa"),  # Edição de tarefa
+    path('tarefas/remover/<int:id>/', remover_tarefa, name="remover_tarefa"),  # Remoção de tarefa
+    path('usuarios/cadastrar/', cadastrar_usuario, name="cadastrar_usuario"),  # Cadastro de usuário
+    path('usuarios/logout/', deslogar_usuario, name="deslogar_usuario"),  # Logout do usuário
+]
---
+++ gerenciador-tarefas/app/views/usuario_views.py
@@ -0,0 +1,44 @@
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required 
+
+
+@login_required
+def dashboard_view(request):
+    return render(request, 'dashboard.html')
+
+def cadastro_usuarios_view(request):
+    return render(request, 'cadastro_usuarios.html')
+
+def cadastrar_usuario(request):
+    if request.method == "POST":
+        form_usuario = UserCreationForm(request.POST)
+        if form_usuario.is_valid():
+            form_usuario.save()
+            return redirect('listar_tarefas')
+    else:
+        form_usuario = UserCreationForm()
+    return render(request, 'usuarios/form_usuario.html', {"form_usuario": form_usuario})
+
+
+def logar_usuario(request):
+    if request.method == "POST":
+        username = request.POST["username"]
+        password = request.POST["password"]
+        usuario = authenticate(request, username=username, password=password)
+        if usuario is not None:
+            login(request, usuario)
+            return redirect('dashboard')  # Redireciona para o dashboard após login
+        else:
+            messages.error(request, 'As credenciais de usuário estão incorretas')
+            return redirect('logar_usuario')
+    else:
+        form_login = AuthenticationForm()
+    return render(request, 'usuarios/login.html', {"form_login": form_login})
+
+
+def deslogar_usuario(request):
+    logout(request)
+    return redirect('logar_usuario')
